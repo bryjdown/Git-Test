bf154ef264e437d33d3deaff8c05621cb01a2fed
bryjdown <bryjdown@ut.utm.edu>
2018-11-25 19:02:36 -0600

    Adding sum function, user input

--- main.cpp
+++ main.cpp
@@ -2,7 +2,26 @@
 
 using namespace std;
 
+int sum(int n);
+
 int main(){
-  cout << "Hello World!" << endl;
+  int n = 0;
+  
+  cout << "Please enter a number: ";
+  cin >> n;
+
+  cout << "The sum of integers from 1 to " << n
+       << " is: " << sum(n) << endl;
+  
   return 0;
+}
+
+int sum(int n){
+  int s = 0;
+
+  for(int i = 1; i <= n; i++){
+    s += i;
+  }
+
+  return s;
 }
